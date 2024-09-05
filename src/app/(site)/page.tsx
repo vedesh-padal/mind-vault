8a067338bfe31e7fbdcd1bd982131537fa2ac5c7
vedesh-padal <vedeshpadal@gmail.com>
2024-09-05 22:40:38 +0530

    eslint setup with basic home page work start

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,16 +1,53 @@
 import TitleSection from "@/components/landing-page/title-section";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
+import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
   return (
-    <section>
-      <div className="overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+    <>
+      <section className="mt-10 gap-4 overflow-hidden px-4 sm:flex sm:flex-col sm:px-6 md:items-center md:justify-center">
         <TitleSection
           pill="✨ Your Workspace, Perfected"
           title="All-In-One Collaboration and Productivity Platform"
         />
-      </div>
-    </section>
+        <div className="mt-6 rounded-xl bg-white bg-gradient-to-r from-green-400 to-brand-primaryBlue p-[2px] sm:w-[300px]">
+          <Button
+            variant="secondary"
+            className="w-full rounded-[10px] p-6 text-2xl bg-background"
+          >
+            Get MindVault Free
+          </Button>
+        </div>
+        <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
+          <Image src={Banner} alt="Application Banner" />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+
+      <section className="relative">
+        <div className="overflow-hidden flex after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:w-20 after:absolute after:z-10
+          before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:bottom-0 before:w-20 before:z-10 before:absolute
+        ">{[...Array(2)].map((arr) => <div key={arr} className="flex flex-nowrap animate-slide"></div>)}
+
+          {CLIENTS.map((client) => (
+            <div
+              key={client.alt}
+              className="relative w-[200px] m-20 shrink-0 flex items-center"
+            >
+              <Image
+                src={client.logo}
+                alt={client.alt}
+                width={200}
+              />
+            </div>
+          ))}
+
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/landing-page/title-section.tsx
+++ src/components/landing-page/title-section.tsx
@@ -13,22 +13,22 @@ const TitleSection: React.FC<TitleSectionProps> = ({
 }) => {
   return (
     <React.Fragment>
-      <section className="flex flex-col gap-4 justify-center items-start md:items-center">
+      <section className="flex flex-col items-start justify-center gap-4 md:items-center">
         <article className="rounded-full p-[1px] text-sm dark:bg-gradient-to-r dark:from-brand-primaryBlue dark:to-brand-primaryPurple">
           <div className="rounded-full px-3 py-1 dark:bg-black">{pill}</div>
         </article>
         {subheading ? (
           <>
-            <h2 className="text-left text-3xl sm:text-5xl sm:max-w-[750px] md:text-center font-semibold">
+            <h2 className="text-left text-3xl font-semibold sm:max-w-[750px] sm:text-5xl md:text-center">
               {title}
             </h2>
 
-            <p className="dark:text-washed-purple-700 sm:max-w-[450px] text-center">
+            <p className="text-center dark:text-washed-purple-700 sm:max-w-[450px]">
               {subheading}
             </p>
           </>
         ) : (
-          <h1 className="text-left text-4xl sm:text-6xl sm:max-w-[850px] md:text-center">
+          <h1 className="text-left text-4xl sm:max-w-[850px] sm:text-6xl md:text-center">
             {title}
           </h1>
         )}
